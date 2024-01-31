26582c9ae85f87641c25709dcdda487ddd0547a4
QuocHung <hungthanh1412hz@gmail.com>
2024-01-31 22:00:43 +0700

    add btn delete

--- src/components/mainPage/components/Main/TodoList/TodoList.jsx
+++ src/components/mainPage/components/Main/TodoList/TodoList.jsx
@@ -1,11 +1,11 @@
 // LIB
-import clsx from 'clsx';
+import clsx from "clsx";
 import { memo, useState } from "react";
 
 // CSS
 import style from "./TodoList.module.scss";
 
-function TodoList({ id, title }) {
+function TodoList({ id }) {
   const [content, setContent] = useState("");
   const [job, setJob] = useState([]);
 
@@ -13,6 +13,10 @@ function TodoList({ id, title }) {
   //   id: id,
   //   title: title,
   // };
+
+  // useLayoutEffect(() => {
+  //   setJob(job);
+  // }, [job]);
 
   const handleSubmit = () => {
     setJob((prev) => [...prev, content]);
@@ -24,7 +28,13 @@ function TodoList({ id, title }) {
 
   return (
     <div className={style.container} id={id}>
-      <h1 className={style.title}>{title || "Title!"}</h1>
+      <h1
+        className={style.title}
+        suppressContentEditableWarning={true}
+        contentEditable={true}
+      >
+        {"Click here to edit title!"}
+      </h1>
       <button className={style.btnAddWork} onClick={handleSubmit}>
         <p className={style.btnText}>+</p>
       </button>
@@ -35,16 +45,29 @@ function TodoList({ id, title }) {
           setContent(e.target.value);
         }}
         value={content}
+        placeholder={"Put your jobs there..."}
       />
       <div className={style.containerJob}>
         <ul>
           {job &&
             job.map((value) => (
               <div className={style.job} key={value}>
-                <label className={clsx(style.containerInput, 'd-flex-c_vertical')}>
+                <label
+                  className={clsx(style.containerInput, "d-flex-c_vertical")}
+                >
                   <input id={"check"} type={"checkbox"} />
                   {value}
                 </label>
+                <div
+                  className={clsx(style.btnDelete, "d-flex-c")}
+                  onClick={() => {
+                    job.splice(job.indexOf(value), 1);
+                    console.log(job);
+                    setJob(prev => [...prev])
+                  }}
+                >
+                  X
+                </div>
               </div>
             ))}
         </ul>
